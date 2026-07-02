96dfca2b2d4bca7b84bd43f6ae53e0a04a916a81
jatin <jatinkumawat530@gmail.com>
2026-07-02 22:03:39 +0530

    changes

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const { parse } = require('csv-parse');
+
+const habitablePlanets = [];
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED' 
+    && planet['koi_insol'] > 0.36 
+    && planet['koi_insol'] < 1.11 
+    && planet['koi_prad'] < 1.6;
+}
+fs.createReadStream('kepler_data.csv')
+  .pipe(
+    parse({
+      comment: '#',
+      columns: true,
+    })
+  )
+  .on('data', (data) => {
+    if(isHabitablePlanet(data)){
+      habitablePlanets.push(data);
+    }
+  })
+  .on('error', (err) => {
+    console.error(err);
+  })
+  .on('end', () => {
+    console.log(habitablePlanets);
+  });
